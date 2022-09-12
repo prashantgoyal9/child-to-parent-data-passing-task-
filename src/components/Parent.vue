509ec18339c8f00ca0b6e7cdd61ead3bafeda363
Prashant Goyal <84865931+prashantgoyal9@users.noreply.github.com>
2022-09-12 20:24:56 +0530

    Add files via upload

---
+++ src/components/Parent.vue
@@ -0,0 +1,35 @@
+<template>
+    
+    <TheChild :getName="getData" />
+ 
+
+    
+</template>
+<script>
+
+import TheChild from './TheChild.vue';
+export default {
+    name:"parent",
+    components:{
+        TheChild
+ 
+},
+data(){
+    return{
+        childName:''
+    }
+},
+methods:{
+        getData(name){
+            this.childName=name
+        }
+    }
+}
+</script>
+<style>
+    *{
+        margin: 0px;
+        padding: 0px;
+    }
+
+</style>
